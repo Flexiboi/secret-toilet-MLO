e8010a10c4a1786f5ff88cc4f630210f105d876d
Flex <45070800+Flexiboi@users.noreply.github.com>
2026-02-28 01:13:46 +0100

    Add files via upload

---
+++ secret_toilet/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Flex'
+description 'Secret Toilet'
+version '1.0.0'
+
+this_is_a_map "yes"
+
+data_file 'TIMECYCLEMOD_FILE' 'deamon_lightcycle.xml'
+data_file 'TIMECYCLEMOD_FILE' 'brightness.xml'
+
+files {
+    'deamon_lightcycle.xml',
+    'brightness.xml',
+    'stream/prop_demon_energy_can.ytyp',
+    'stream/*'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_demon_energy_can.ytyp'
+dependency '/assetpacks'
+
+dependency '/assetpacks'
